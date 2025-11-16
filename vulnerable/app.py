4a0d7f8da1834af854ced0c7a9f828e2d31f1ff7
idirley soares <idirley.soares@gmail.com>
2025-11-16 13:08:51 -0300

    p

--- vulnerable/app.py
+++ vulnerable/app.py
@@ -7,17 +7,19 @@ O objetivo é que o CodeQL detecte e a pipeline bloqueie o deploy para o stage.
 
 import sqlite3
 
+
 def busca_usuario():
     conn = sqlite3.connect(":memory:")
     cur = conn.cursor()
-    cur.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)")
-    cur.execute("INSERT INTO users (username, password) VALUES ('admin', 'admin123')")
+    cur.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, user TEXT, passw TEXT)")
+    cur.execute("INSERT INTO users (user, passw) VALUES ('admin', 'admin123')")
     usuario = input("Digite o nome de usuário: ")
     # Vulnerabilidade: SQL Injection
-    query = f"SELECT * FROM users WHERE username = '{usuario}'"
+    query = f"SELECT * FROM users WHERE user = '{usuario}'"
     print(f"Executando: {query}")
     cur.execute(query)
     print(cur.fetchall())
+
 
 if __name__ == "__main__":
     busca_usuario()
